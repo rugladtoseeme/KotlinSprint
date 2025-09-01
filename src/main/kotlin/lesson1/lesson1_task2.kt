f6065d5c43e02ddacb250680b90fa01b76ec66fe
rugladtoseeme <mrgrafen@mail.ru>
2025-09-01 13:41:50 +0300

    KS-1-2 created new variables for online shop and decremented a number of employees and printed all values

---
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -0,0 +1,18 @@
+package org.example.lesson1
+
+fun main() {
+
+    val numberOfOrders: Int = 75
+    val thanksForPurchaseText: String = "Спасибо за ваш заказ!"
+
+    println(numberOfOrders)
+    println(thanksForPurchaseText)
+
+    var numberOfOnlineShopEmployees: Int = 2000
+
+    //println(numberOfOnlineShopEmployees)
+
+    numberOfOnlineShopEmployees--
+
+    println(numberOfOnlineShopEmployees)
+}
