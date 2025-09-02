c63fef5fd3607502c2ad21a5346d8c9ce379da85
rugladtoseeme <mrgrafen@mail.ru>
2025-09-02 15:33:10 +0300

    KS-3-1 вывести приветствие в зависимости от времени суток

---
+++ src/main/kotlin/lesson3/lesson3_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson3
+
+fun main(){
+
+    val name = "Имя"
+
+    var greeting = "Добрый день, $name!"
+    println(greeting)
+
+    greeting = "Добрый вечер, $name!"
+    println(greeting)
+}
