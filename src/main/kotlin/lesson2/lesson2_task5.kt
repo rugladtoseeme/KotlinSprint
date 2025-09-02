116f38accaf66e0e433b8baf91c6693eb56677a0
rugladtoseeme <mrgrafen@mail.ru>
2025-09-02 14:33:56 +0300

    KS-2-5 поменяла чтобы без цикла

--- src/main/kotlin/lesson2/lesson2_task5.kt
+++ src/main/kotlin/lesson2/lesson2_task5.kt
@@ -1,14 +1,16 @@
 package org.example.lesson2
 
-fun main(){
+import kotlin.math.pow
+
+fun main() {
 
     var sumOfDeposit = 70000.0
     val interestRate = 0.167
     val yearsOfDeposit = 20
 
-    for (i in 1 .. yearsOfDeposit){
-        sumOfDeposit *= (1+interestRate)
-    }
+    val coeff = 1 + interestRate
+
+    sumOfDeposit *= coeff.pow(yearsOfDeposit)
 
     print(String.format("%.3f", sumOfDeposit))
 }
