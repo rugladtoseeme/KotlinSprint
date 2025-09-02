a9c69fe68112f81e7cf2ecc5a1706313e3ff2965
rugladtoseeme <mrgrafen@mail.ru>
2025-09-02 15:49:24 +0300

    KS-3-2 вывести данные - фио и возраст - до и после брака

---
+++ src/main/kotlin/lesson3/lesson3_task2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson3
+
+fun main(){
+
+    val surnameBefore = "Андреева"
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+    val ageBefore = 20
+
+    val surnameAfter = "Сидорова"
+    val ageAfter = 22
+
+    println("$surnameBefore $name $patronymic, $ageBefore")
+    println("$surnameAfter $name $patronymic, $ageAfter")
+
+}
