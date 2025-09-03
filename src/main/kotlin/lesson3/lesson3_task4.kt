c5388901abf595a6d2f6d7f5fa4b70dd9ae8920c
rugladtoseeme <mrgrafen@mail.ru>
2025-09-03 15:08:24 +0300

    KS-3-4 правки

--- src/main/kotlin/lesson3/lesson3_task4.kt
+++ src/main/kotlin/lesson3/lesson3_task4.kt
@@ -4,13 +4,13 @@ fun main(){
 
     var chessPositionFrom = "E2"
     var chessPositionTo = "E4"
-    var chessTurnNumber = 1
+    var chessTurnNumber = 0
 
     println("$chessPositionFrom-$chessPositionTo;$chessTurnNumber")
 
     chessPositionFrom = "D2"
     chessPositionTo = "D3"
-    chessTurnNumber = 2
+    chessTurnNumber = 1
 
     println("$chessPositionFrom-$chessPositionTo;$chessTurnNumber")
 }
