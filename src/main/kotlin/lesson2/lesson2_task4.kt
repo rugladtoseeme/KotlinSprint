77090c2784d07758a2bdf2bba34906332fcdd67f
rugladtoseeme <mrgrafen@mail.ru>
2025-09-03 20:06:20 +0300

    KS-2-4 правки по ревью

--- src/main/kotlin/lesson2/lesson2_task4.kt
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -7,9 +7,10 @@ fun main() {
     val buffPercents = 20
     val resourceCrystalAmountRaw = 7
     val resourceIronAmountRaw = 11
+    val hundredPercents = 100
 
-    val resourceCrystalAmountBuffed = floor(resourceCrystalAmountRaw * 1.0 / 100 * buffPercents).toInt()
-    val resourceIronAmountBuffed = floor(resourceIronAmountRaw * 1.0 / 100 * buffPercents).toInt()
+    val resourceCrystalAmountBuffed = floor(resourceCrystalAmountRaw * 1.0 / hundredPercents * buffPercents).toInt()
+    val resourceIronAmountBuffed = floor(resourceIronAmountRaw * 1.0 / hundredPercents * buffPercents).toInt()
 
     println("получено $resourceCrystalAmountBuffed бонусных единиц кристаллической руды!")
     println("получено $resourceIronAmountBuffed бонусных единиц железной руды!")
