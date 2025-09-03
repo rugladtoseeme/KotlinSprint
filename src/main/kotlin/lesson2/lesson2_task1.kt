6799cbd34c41c0839f528ec8383a99ca6393848c
rugladtoseeme <mrgrafen@mail.ru>
2025-09-03 13:59:26 +0300

    KS-2-1 правки по ревью

--- src/main/kotlin/lesson2/lesson2_task1.kt
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -1,11 +1,13 @@
 package org.example.lesson2
 
-fun main(){
+fun main() {
 
     val grade1 = 3
     val grade2 = 4
     val grade3 = 3
     val grade4 = 5
 
-    println((grade1+grade2+grade3+grade4)/4.0)
+    val numberOfStudents = 4
+
+    println((grade1 + grade2 + grade3 + grade4) * 1.0 / numberOfStudents)
 }
