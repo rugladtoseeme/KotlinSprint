5db07d9be493b5e15e082623141f917d1026f175
rugladtoseeme <mrgrafen@mail.ru>
2025-09-03 19:52:27 +0300

    KS-2-4 правки по ревью

--- src/main/kotlin/lesson2/lesson2_task4.kt
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -4,12 +4,12 @@ import kotlin.math.floor
 
 fun main() {
 
-    val buffCoeff = 0.2
+    val buffPercents = 20
     val resourceCrystalAmountRaw = 7
     val resourceIronAmountRaw = 11
 
-    val resourceCrystalAmountBuffed = floor(resourceCrystalAmountRaw * (buffCoeff)).toInt()
-    val resourceIronAmountBuffed = floor(resourceIronAmountRaw * (buffCoeff)).toInt()
+    val resourceCrystalAmountBuffed = floor(resourceCrystalAmountRaw * 1.0 / 100 * buffPercents).toInt()
+    val resourceIronAmountBuffed = floor(resourceIronAmountRaw * 1.0 / 100 * buffPercents).toInt()
 
     println("получено $resourceCrystalAmountBuffed бонусных единиц кристаллической руды!")
     println("получено $resourceIronAmountBuffed бонусных единиц железной руды!")
