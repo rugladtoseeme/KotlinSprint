dfea9d7f6d261605723e6a58f91f295d4297d8c8
rugladtoseeme <mrgrafen@mail.ru>
2025-09-05 22:40:16 +0300

    KS-4-3 правки по ревью

--- src/main/kotlin/lesson4/lesson4_task3.kt
+++ src/main/kotlin/lesson4/lesson4_task3.kt
@@ -1,23 +1,23 @@
 package org.example.lesson4
 
+const val IS_SUNNY = true
+const val IS_TENT_OPEN = true
+const val BEANS_IDEAL_HUMIDITY_PERCENTAGE = 20
+const val BEANS_WORST_SEASON = "winter"
+
 fun main() {
 
-    val beansIdealWeatherIsSunny = true
-    val beansIdealConditionIsTentOpened = true
-    val beansIdealConditionHumidityPercentage = 20
-    val beansWorstSeason = "winter"
-
-    val beansTodaysWeatherIsSunny = true
-    val beansTodaysConditionIsTentOpened = true
-    val beansTodaysConditionHumidityPercentage = 20
+    val isSunnyToday = true
+    val isTentOpenToday = true
+    val beansTodaysHumidityPercentage = 20
     val beansTodaysSeason = "winter"
 
     println(
         "Благоприятные ли условия сейчас для роста бобовых? ${
-            beansTodaysWeatherIsSunny == beansIdealWeatherIsSunny &&
-                    beansTodaysConditionIsTentOpened == beansIdealConditionIsTentOpened &&
-                    beansTodaysConditionHumidityPercentage == beansIdealConditionHumidityPercentage &&
-                    beansTodaysSeason != beansWorstSeason
+            isSunnyToday == IS_SUNNY &&
+                    isTentOpenToday == IS_TENT_OPEN &&
+                    beansTodaysHumidityPercentage == BEANS_IDEAL_HUMIDITY_PERCENTAGE &&
+                    beansTodaysSeason != BEANS_WORST_SEASON
         }"
     )
 }
