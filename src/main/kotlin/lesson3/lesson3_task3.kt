a86932b3d94b0ca119801c7d93db923abe393abc
rugladtoseeme <mrgrafen@mail.ru>
2025-09-05 14:31:09 +0300

    KS-3-3 правки

--- src/main/kotlin/lesson3/lesson3_task3.kt
+++ src/main/kotlin/lesson3/lesson3_task3.kt
@@ -4,16 +4,26 @@ fun main() {
 
     val number = 9
 
+    val one = 1
+    val two = 2
+    val three = 3
+    val four = 4
+    val five = 5
+    val six = 6
+    val seven = 7
+    val eight = 8
+    val nine = 9
+
     val multiplicationTableText = """
-        $number * 1 = ${number * 1}
-        $number * 2 = ${number * 2}
-        $number * 3 = ${number * 3}
-        $number * 4 = ${number * 4}
-        $number * 5 = ${number * 5}
-        $number * 6 = ${number * 6}
-        $number * 7 = ${number * 7}
-        $number * 8 = ${number * 8}
-        $number * 9 = ${number * 9}
+        $number * 1 = ${number * one}
+        $number * 2 = ${number * two}
+        $number * 3 = ${number * three}
+        $number * 4 = ${number * four}
+        $number * 5 = ${number * five}
+        $number * 6 = ${number * six}
+        $number * 7 = ${number * seven}
+        $number * 8 = ${number * eight}
+        $number * 9 = ${number * nine}
     """.trimIndent()
 
     println(multiplicationTableText)
