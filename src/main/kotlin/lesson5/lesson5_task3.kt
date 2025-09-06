9f6f484f08e5da6dd7317aa27c5d17af8dab38be
rugladtoseeme <mrgrafen@mail.ru>
2025-09-06 23:59:21 +0300

    KS-5-3 правки по ревью

--- src/main/kotlin/lesson5/lesson5_task3.kt
+++ src/main/kotlin/lesson5/lesson5_task3.kt
@@ -1,15 +1,14 @@
 package org.example.lesson5
 
 import kotlin.random.Random
-import kotlin.random.nextInt
 
 fun main() {
 
-    val secretNumber1 = Random.nextInt(42)
-    var secretNumber2 = Random.nextInt(42)
+    val secretNumber1 = Random.nextInt(43)
+    var secretNumber2 = Random.nextInt(43)
 
     while (secretNumber2 == secretNumber1)
-        secretNumber2 = Random.nextInt(42)
+        secretNumber2 = Random.nextInt(43)
 
     println("Введите число от 1 до 42!")
     val inputNumber1 = readln().toInt()
@@ -17,18 +16,15 @@ fun main() {
     println("Введите число от 1 до 42!")
     val inputNumber2 = readln().toInt()
 
-    val strResult = if ((inputNumber1 == secretNumber1 && inputNumber2 == secretNumber2) ||
-        (inputNumber2 == secretNumber1 && inputNumber1 == secretNumber2)
-    )
-        "Поздравляем! Вы выиграли главный приз!"
-    else if ((inputNumber1 == secretNumber1 && inputNumber2 != secretNumber2) ||
-        (inputNumber2 != secretNumber1 && inputNumber1 == secretNumber2) ||
-        (inputNumber1 != secretNumber1 && inputNumber2 == secretNumber2) ||
-        (inputNumber2 == secretNumber1 && inputNumber1 != secretNumber2)
-    )
-        "Вы выиграли утешительный приз!"
-    else "Неудача!"
+    val firstRight = if (inputNumber1 == secretNumber1 || inputNumber1 == secretNumber2) 1 else 0
+    val secondRight = if (inputNumber2 == secretNumber1 || inputNumber2 == secretNumber2) 1 else 0
+    val result = firstRight + secondRight
 
-    println(strResult)
+    println(
+        if (result == 0) "Неудача!"
+        else if (result == 1) "Вы выиграли утешительный приз!"
+        else "Поздравляем! Вы выиграли главный приз!"
+    )
+
     println("Правильные ответы: $secretNumber1, $secretNumber2")
 }
