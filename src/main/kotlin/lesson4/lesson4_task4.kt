bb32c191f69547a0da25303fbdd3f5afc835ac17
rugladtoseeme <mrgrafen@mail.ru>
2025-09-06 21:15:32 +0300

    KS-4-4 правки по ревью

--- src/main/kotlin/lesson4/lesson4_task4.kt
+++ src/main/kotlin/lesson4/lesson4_task4.kt
@@ -2,18 +2,15 @@ package org.example.lesson4
 
 fun main() {
 
-    val isFirstDayTrainingArmsAndPress = true
-
     val todaysDayNumber = 5
     val isDayEven = todaysDayNumber % 2 == 0
 
-
     println(
         """
-        Упражнения для рук:       ${isDayEven != isFirstDayTrainingArmsAndPress}
-        Упражнения для ног:       ${isDayEven == isFirstDayTrainingArmsAndPress}
-        Упражнения для спины:  ${isDayEven == isFirstDayTrainingArmsAndPress}
-        Упражнения для пресса: ${isDayEven != isFirstDayTrainingArmsAndPress}
+        Упражнения для рук:       ${!isDayEven}
+        Упражнения для ног:       $isDayEven
+        Упражнения для спины:  $isDayEven
+        Упражнения для пресса: ${!isDayEven}
     """.trimIndent()
     )
 }
