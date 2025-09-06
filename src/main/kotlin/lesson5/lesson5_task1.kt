6ab45664da2330471bdfffddaac06fa84bed3e3b
rugladtoseeme <mrgrafen@mail.ru>
2025-09-06 21:24:48 +0300

    KS-5-1 правки по ревью

--- src/main/kotlin/lesson5/lesson5_task1.kt
+++ src/main/kotlin/lesson5/lesson5_task1.kt
@@ -1,18 +1,17 @@
 package org.example.lesson5
 
+import kotlin.random.Random
+
 fun main() {
 
-    val number1 = 3
-    val number2 = 5
-
-    val sumReal = number1 + number2
+    val number1 = Random.nextInt(10)
+    val number2 = Random.nextInt(10)
 
     println("$number1 + $number2 = ?")
-    val sumInput = readln().toInt()
+    val answer = readln().toInt()
 
-    if (sumInput == sumReal)
+    if (answer == number1 + number2)
         println("Добро пожаловать!")
     else
         println("Доступ запрещен.")
-
 }
