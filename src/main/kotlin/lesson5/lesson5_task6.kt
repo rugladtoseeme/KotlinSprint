27a77c258d95cf7c7511ed2757cc66b2edaa7a67
rugladtoseeme <mrgrafen@mail.ru>
2025-09-07 19:56:21 +0300

    KS-5-6 калькулятор ИМТ

--- src/main/kotlin/lesson5/lesson5_task6.kt
+++ src/main/kotlin/lesson5/lesson5_task6.kt
@@ -14,21 +14,21 @@ fun main() {
 
     val IMT = weight / heightMeters.pow(2)
 
-    var weightCategoryText = ""
+    var weightCategory = ""
     if (IMT < 18.5) {
         println("ИМТ меньше 18.5: Недостаточная масса тела.")
-        weightCategoryText = "Недостаточная масса тела"
+        weightCategory = "Недостаточная масса тела"
     } else if (IMT in 18.5..25.0) {
         println("ИМТ между 18.5 и 25: Нормальная масса тела.")
-        weightCategoryText = "Нормальная масса тела"
+        weightCategory = "Нормальная масса тела"
     } else if (IMT in 25.0..30.0) {
         println("ИМТ между 25.0 и 30: Избыточная масса тела.")
-        weightCategoryText = "Избыточная масса тела"
+        weightCategory = "Избыточная масса тела"
     } else {
         println("ИМТ более 30: Ожирение.")
-        weightCategoryText = "Ожирение"
+        weightCategory = "Ожирение"
     }
 
-    println("Ваш ИМТ составляет ${String.format("%.2f", IMT)}. Диагноз: $weightCategoryText")
+    println("Ваш ИМТ составляет ${String.format("%.2f", IMT)}. Диагноз: $weightCategory.")
 
 }
