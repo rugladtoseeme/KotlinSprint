b533ae2825d083111e87636bdb34db4de3d025a9
rugladtoseeme <mrgrafen@mail.ru>
2025-09-07 23:05:53 +0300

    KS-5-5 правки по ревью

--- src/main/kotlin/lesson5/lesson5_task5.kt
+++ src/main/kotlin/lesson5/lesson5_task5.kt
@@ -1,37 +1,46 @@
 package org.example.lesson5
 
+fun generateNumber(): Int {
+    return (0..42).random()
+}
+
+fun enterNumber(): Int {
+    println("Введите число от 0 до 42!")
+    return readln().toInt()
+}
+
 fun main() {
 
-    val secretNumber1 = (0..42).random()
-    var secretNumber2 = (0..42).random()
+    val secretNumber1 = generateNumber()
+    var secretNumber2 = generateNumber()
     while (secretNumber2 == secretNumber1)
-        secretNumber2 = (0..42).random()
+        secretNumber2 = generateNumber()
 
-    var secretNumber3 = (0..42).random()
+    var secretNumber3 = generateNumber()
 
     while (secretNumber3 == secretNumber1 || secretNumber3 == secretNumber2)
-        secretNumber3 = (0..42).random()
+        secretNumber3 = generateNumber()
 
     val secretList = listOf(secretNumber1, secretNumber2, secretNumber3)
 
-    println("Введите число от 0 до 42!")
-    val inputNumber1 = readln().toInt()
+    val inputNumber1 = enterNumber()
 
-    println("Введите число от 0 до 42!")
-    val inputNumber2 = readln().toInt()
+    val inputNumber2 = enterNumber()
 
-    println("Введите число от 0 до 42!")
-    val inputNumber3 = readln().toInt()
+    val inputNumber3 = enterNumber()
 
     val inputList = listOf(inputNumber1, inputNumber2, inputNumber3)
 
     val rightAnswersListSize = inputList.intersect(secretList).size
 
     println(
-        if (rightAnswersListSize == 0) "Неудача! Вы не угадали ни одного числа."
-        else if (rightAnswersListSize == 1) "Вы угадали одно число и выиграли утешительный приз!"
-        else if (rightAnswersListSize == 2) "Поздравляем! Вы угадали два числа и выиграли крупный приз!"
-        else "Поздравляем! Вы угадали все числа и выиграли джекпот!"
+        when (rightAnswersListSize) {
+            0 -> "Неудача! Вы не угадали ни одного числа."
+            1 -> "Вы угадали одно число и выиграли утешительный приз!"
+            2 -> "Поздравляем! Вы угадали два числа и выиграли крупный приз!"
+            3 -> "Поздравляем! Вы угадали все числа и выиграли джекпот!"
+            else -> "Поздравляем! Вы угадали все числа и выиграли джекпот!"
+        }
     )
 
     println("Правильные ответы: $secretNumber1, $secretNumber2, $secretNumber3")
