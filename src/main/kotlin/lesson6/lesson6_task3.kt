11b52ff4a08978297bec9394a308df7b2596a976
rugladtoseeme <mrgrafen@mail.ru>
2025-09-11 16:32:33 +0300

    KS-6-3 правки

--- src/main/kotlin/lesson6/lesson6_task3.kt
+++ src/main/kotlin/lesson6/lesson6_task3.kt
@@ -1,12 +1,14 @@
 package org.example.lesson6
 
+const val MILLISECONDS_IN_SECOND = 1000L
+
 fun main() {
     println("Введите количество секунд")
     var seconds = readln().toInt()
 
     while (seconds > 0) {
         println("Осталось секунд: ${seconds--}.")
-        Thread.sleep(1000)
+        Thread.sleep(MILLISECONDS_IN_SECOND)
     }
 
     println("Время вышло.")
