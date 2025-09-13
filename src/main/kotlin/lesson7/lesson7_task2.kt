922b193b8b144708504402e959ee0b3eba245777
rugladtoseeme <mrgrafen@mail.ru>
2025-09-13 14:44:08 +0300

    KS-7-2 вход по смс с повторной высылкой кода

---
+++ src/main/kotlin/lesson7/lesson7_task2.kt
@@ -0,0 +1,23 @@
+package org.example.lesson7
+
+private const val MIN_NUMBER = 1000
+private const val MAX_NUMBER = 9999
+
+fun main() {
+
+    do {
+        val codeGenerated = (MIN_NUMBER..MAX_NUMBER).random()
+        println("Ваш код для входа: $codeGenerated")
+        println("Введите код")
+        val codeInput = readln().toInt()
+        val isCodeNotCorrect = codeInput != codeGenerated
+        if (isCodeNotCorrect){
+            println("Неверный код! Попробуйте снова, мы вышлем вам код повторно.")
+        }
+        else{
+            println("Код верный. Добро подаловать в приложение!")
+            return
+        }
+    } while (isCodeNotCorrect)
+
+}
