8207d34036e4dca0981dbe8cba1182bf5c0c1e1b
rugladtoseeme <mrgrafen@mail.ru>
2025-09-13 14:36:38 +0300

    KS-7-1 правки

--- src/main/kotlin/lesson7/lesson7_task1.kt
+++ src/main/kotlin/lesson7/lesson7_task1.kt
@@ -9,8 +9,8 @@ fun main() {
 
     var password = ""
     for (i in 1..3) {
-        password = "$password${Char(Random.nextInt(ASCII_CODE_A, ASCII_CODE_Z))}"
-        password = "$password${Random.nextInt(0, 9)}"
+        password = "$password${Char((ASCII_CODE_A.. ASCII_CODE_Z).random())}"
+        password = "$password${(0..9).random()}"
     }
     println("сгенерирован пароль: $password")
 }
