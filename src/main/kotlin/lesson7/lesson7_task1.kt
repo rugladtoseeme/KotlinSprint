c126de1a6f701a0908a96fa64a110844dd961cf5
rugladtoseeme <mrgrafen@mail.ru>
2025-09-13 16:34:05 +0300

    KS-7-1 правки

--- src/main/kotlin/lesson7/lesson7_task1.kt
+++ src/main/kotlin/lesson7/lesson7_task1.kt
@@ -1,15 +1,10 @@
 package org.example.lesson7
 
-import kotlin.random.Random
-
-const val ASCII_CODE_A = 97
-const val ASCII_CODE_Z = 122
-
 fun main() {
 
     var password = ""
     for (i in 1..3) {
-        password = "$password${Char((ASCII_CODE_A..ASCII_CODE_Z).random())}"
+        password = "$password${('a'..'z').random()}"
         password = "$password${(0..9).random()}"
     }
     println("сгенерирован пароль: $password")
