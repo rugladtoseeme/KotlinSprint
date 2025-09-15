7af4a5fe7d17b0c8f004df564e5ad207decccad9
rugladtoseeme <mrgrafen@mail.ru>
2025-09-15 12:39:54 +0300

    KS-8-1 правки

--- src/main/kotlin/lesson8/lesson8_task1.kt
+++ src/main/kotlin/lesson8/lesson8_task1.kt
@@ -2,10 +2,6 @@ package org.example.lesson8
 
 fun main() {
 
-    val viewsForOneWeekDaily = arrayOf(1, 4, 7, 6, 2, 9, 5)
-    var totalViewsForAWeek = 0
-    for (i in viewsForOneWeekDaily) {
-        totalViewsForAWeek += i
-    }
-    println("Всего за неделю было $totalViewsForAWeek просмотров рекламы.")
+    val adViewsForOneWeek = intArrayOf(1, 4, 7, 6, 2, 9, 5).sum()
+    println("Всего за неделю было $adViewsForOneWeek просмотров рекламы.")
 }
