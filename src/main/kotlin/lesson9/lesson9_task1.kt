e3879d5416b6ca057cf71efd25ae97ee6a0dbd92
rugladtoseeme <mrgrafen@mail.ru>
2025-09-16 12:46:03 +0300

    KS-9-1 правки по ревью

--- src/main/kotlin/lesson9/lesson9_task1.kt
+++ src/main/kotlin/lesson9/lesson9_task1.kt
@@ -2,9 +2,9 @@ package org.example.lesson9
 
 fun main() {
 
-    val ingredientsList = listOf("рис", "нори", "лосось", "соус", "кунжут", "огурец", "сыр сливочный")
-    println("В рецепте есть следующие ингредиенты: ${ingredientsList.joinToString(", ", postfix = ".")}")
-    ingredientsList.forEach {
+    val ingredients = listOf("рис", "нори", "лосось", "соус", "кунжут", "огурец", "сыр сливочный")
+    println("В рецепте есть следующие ингредиенты: ${ingredients.joinToString(", ", postfix = ".")}")
+    ingredients.forEach {
         println(it)
     }
 }
