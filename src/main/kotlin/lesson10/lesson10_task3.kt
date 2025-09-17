72c228b6c91577863e0840bbed69f1124585af13
rugladtoseeme <mrgrafen@mail.ru>
2025-09-17 12:00:28 +0300

    KS-10-3 правки

--- src/main/kotlin/lesson10/lesson10_task3.kt
+++ src/main/kotlin/lesson10/lesson10_task3.kt
@@ -2,9 +2,11 @@ package org.example.lesson10
 
 fun generatePassword(pwdLength: Int): String {
     var password = ""
+    val digitsRange = '0'..'9'
+    val charsSequence = "!\"#$%&'()*+,-./ "
     for (i in 0..pwdLength) {
-        if (i % 2 == 0) password += (0..9).random()
-        else password += ("!\"#$%&'()*+,-./ ").random()
+        if (i % 2 == 0) password += digitsRange.random()
+        else password += charsSequence.random()
     }
     return password
 }
