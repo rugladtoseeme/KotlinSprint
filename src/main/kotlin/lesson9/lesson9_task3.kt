332730ae4f7b7bf08531cd25b81713911f9ee1c5
rugladtoseeme <mrgrafen@mail.ru>
2025-09-17 11:55:46 +0300

    KS-9-3 правки по ревью

--- src/main/kotlin/lesson9/lesson9_task3.kt
+++ src/main/kotlin/lesson9/lesson9_task3.kt
@@ -2,12 +2,19 @@ package org.example.lesson9
 
 fun main() {
 
-    var ingredientsForOmelet = mutableListOf(2, 50, 15)
+    val ingredientsForOmelet = mutableListOf(2, 50, 15)
+    val eggsIndex = 0
+    val milkIndex = 1
+    val butterIndex = 2
     println("Введите количество порций")
     val numberOfPortions = readln().toInt()
-    for (i in 0..ingredientsForOmelet.size - 1) {
-        ingredientsForOmelet[i] *= numberOfPortions
-    }
-    println("На $numberOfPortions порций омлета требуется ингредиентов: яиц - ${ingredientsForOmelet[0]} шт., молока - ${ingredientsForOmelet[1]} мл., сливочного масла - ${ingredientsForOmelet[2]} г.")
+    val ingredientsForPortions = ingredientsForOmelet.map { it * numberOfPortions }
+
+    println(
+        """На $numberOfPortions порций омлета требуется ингредиентов: яиц - ${ingredientsForPortions[eggsIndex]} шт., 
+            |молока - ${ingredientsForPortions[milkIndex]} мл., 
+            |сливочного масла - ${ingredientsForPortions[butterIndex]} г.""".trimMargin()
+    )
+
 
 }
