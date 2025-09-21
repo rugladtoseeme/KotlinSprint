b472d4a159e9b0b413de9589a0bad06fac55952e
rugladtoseeme <mrgrafen@mail.ru>
2025-09-21 17:08:02 +0300

    KS-11-4 правки по ревью

--- src/main/kotlin/lesson11/lesson11-task4.kt
+++ src/main/kotlin/lesson11/lesson11-task4.kt
@@ -1,22 +1,25 @@
 package org.example.lesson11
 
 class Category(
+    val id: Int,
     val name: String,
     val picture: String,
-    val recipes: MutableList<Recipe>,
+    val recipes: List<Recipe>,
     val description: String,
 )
 
 class Recipe(
+    val id: Int,
     val name: String,
     val picture: String,
     val portionsNumber: Int,
     val ingredients: List<Ingredient>,
+    val cookingMethod: List<String>,
 )
 
 class Ingredient(
     val name: String,
-    val amount: Int,
+    val amount: String,
     val unitOfMeasure: String,
 )
 
