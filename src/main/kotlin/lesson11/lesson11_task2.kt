d2bd15daa13f1ff70940ac5c5eff591f4228125f
rugladtoseeme <mrgrafen@mail.ru>
2025-09-21 13:19:04 +0300

    KS-11-2 создать класс с методами

---
+++ src/main/kotlin/lesson11/lesson11_task2.kt
@@ -0,0 +1,40 @@
+package org.example.lesson11
+
+import kotlin.text.replace
+
+class User2(
+    val id: Int,
+    val username: String,
+    var password: String,
+    val email: String,
+    var bio: String? = null,
+) {
+    fun printUserInfo() = println(
+        """User2: id = $id, username = $username, password = $password, 
+        email = $email, bio = $bio""".replace((Regex("\\s+")), " ")
+    )
+
+    fun setBioFromConsole() {
+        println("Введите bio пользователя:")
+        bio = readln()
+        println("Значение bio установлено успешно.")
+    }
+
+    fun changePassword() {
+        println("Введите старый пароль")
+        if (readln() == password) {
+            println("Введите новый пароль")
+            password = readln()
+            println("Успешно сменен пароль!")
+        } else println("Неверный ввод пароля, доступ запрещен.")
+    }
+}
+
+fun main() {
+    val user = User2(id = 1, username = "user", password = "123", email = "user@mail.ru")
+    user.setBioFromConsole()
+    user.changePassword()
+    user.printUserInfo()
+}
+
+
