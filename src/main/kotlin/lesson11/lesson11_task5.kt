543ad5d6e90c4555d45943e09060a3c8dfc2a69e
rugladtoseeme <mrgrafen@mail.ru>
2025-09-25 13:28:13 +0300

    KS-11-5 правки по ревью

--- src/main/kotlin/lesson11/lesson11_task5.kt
+++ src/main/kotlin/lesson11/lesson11_task5.kt
@@ -4,86 +4,40 @@ class Forum {
     private val members: MutableList<Member> = mutableListOf()
     private val messages: MutableList<Message> = mutableListOf()
 
-    fun createNewUser(username: String): Member {
+    fun createNewUser(_username: String): Member {
         val lastElem = members.lastOrNull()
         val newId = if (lastElem == null) 1 else lastElem.userId + 1
-        val newMember = Member.Builder()
-            .userId(newId)
-            .username(username)
-            .build()
+        val newMember = Member(userId = newId, username = _username)
         members.add(newMember)
         return newMember
     }
 
-    fun createNewMessage(message: String, authorId: Int): Message? {
-        if (members.find { it.userId == authorId } == null) {
+    fun createNewMessage(_message: String, _authorId: Int): Message? {
+        if (members.find { it.userId == _authorId } == null) {
             println("на форуме нет такого зарегистрированного пользователя")
             return null
         }
-        val newMessage = Message.Builder()
-            .authorId(authorId)
-            .message(message)
-            .build()
+        val newMessage = Message(authorId = _authorId, message = _message)
         messages.add(newMessage)
         return newMessage
     }
 
     fun printThread() {
         for (i in messages) {
-            println("автор: ${i.authorId}: сообщение: ${i.message}")
+            println("автор: ${members.find { it.userId == i.authorId }?.username}: сообщение: ${i.message}")
         }
     }
 }
 
 class Member(
-    memberBuilder: Builder,
-) {
-    val userId: Int = memberBuilder.userId
-    val username: String = memberBuilder.username
-
-    class Builder {
-        var userId: Int = 0
-        var username: String = ""
-
-        fun userId(userId: Int): Builder {
-            this.userId = userId
-            return this
-        }
-
-        fun username(username: String): Builder {
-            this.username = username
-            return this
-        }
-
-        fun build(): Member = Member(this)
-    }
-
-}
+    val userId: Int,
+    val username: String,
+)
 
 class Message(
-    messageBuilder: Builder,
-) {
-    val authorId: Int = messageBuilder.authorId
-    val message: String = messageBuilder.message
-
-    class Builder {
-        var authorId: Int = 0
-        var message: String = ""
-
-        fun authorId(authorId: Int): Builder {
-            this.authorId = authorId
-            return this
-        }
-
-        fun message(message: String): Builder {
-            this.message = message
-            return this
-        }
-
-        fun build(): Message = Message(this)
-    }
-
-}
+    val authorId: Int,
+    val message: String
+)
 
 fun main() {
 
