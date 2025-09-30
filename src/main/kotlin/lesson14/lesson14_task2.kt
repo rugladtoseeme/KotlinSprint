948d374ca8c7f5f98eaf18a16a9c8f4d67ae69ca
rugladtoseeme <mrgrafen@mail.ru>
2025-09-30 13:54:24 +0300

    KS-14-2 правки по ревью

--- src/main/kotlin/lesson14/lesson14_task2.kt
+++ src/main/kotlin/lesson14/lesson14_task2.kt
@@ -1,9 +1,9 @@
 package org.example.lesson14
 
 open class Liner1(
-    val speed: UInt = 50u,
-    val passengers: UInt = 500u,
-    val cargoWeight: UInt = 300u,
+    open val speed: Int = 50,
+    open val passengers: Int = 500,
+    open val cargoWeight: Int = 300,
 ) {
     override fun toString(): String {
         return """Лайнер: скорость = $speed км/ч, вмещает $passengers пассажиров и $cargoWeight т груза.
@@ -17,8 +17,8 @@ open class Liner1(
 }
 
 class Icebreaker1(
-    speed: UInt = 30u,
-    passengers: UInt = 50u,
+    speed: Int = 30,
+    passengers: Int = 50,
 ) : Liner1(speed, passengers) {
 
     fun breakIce() {
@@ -37,8 +37,8 @@ class Icebreaker1(
 }
 
 class CargoShip1(
-    passengers: UInt = 50u,
-    cargoWeight: UInt = 1000u,
+    passengers: Int = 50,
+    cargoWeight: Int = 1000,
 ) : Liner1(passengers, cargoWeight) {
     override fun toString(): String {
         return """Грузовой корабль: скорость = $speed км/ч, вмещает $passengers пассажиров и $cargoWeight т груза.
