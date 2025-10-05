09ae135547b81eb8f4a14a4c19113ef8cb6106a6
rugladtoseeme <mrgrafen@mail.ru>
2025-10-05 17:50:28 +0300

    KS-15-3 иерархия пользователей на форуме

---
+++ src/main/kotlin/lesson15/lesson15_task3.kt
@@ -0,0 +1,29 @@
+package org.example.lesson15
+
+abstract class AbstractUser(val username: String) {
+    fun readForum() {
+        println("пользователь $username читает форум")
+    }
+
+    fun writeMessage() {
+        println("пользователь $username печатает сообщение")
+    }
+}
+
+class User(username: String) : AbstractUser(username)
+class Administrator(username: String) : AbstractUser(username) {
+    fun deleteMessage() {
+        println("администратор $username удаляет сообщение")
+    }
+}
+
+
+fun main() {
+
+    val admin = Administrator("anna")
+    val user = User("elena")
+    admin.writeMessage()
+    admin.deleteMessage()
+    user.writeMessage()
+    user.readForum()
+}
