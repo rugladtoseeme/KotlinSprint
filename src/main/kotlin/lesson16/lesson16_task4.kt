6a8451519d09876616a6c9a258f3bb69d2718f42
rugladtoseeme <mrgrafen@mail.ru>
2025-10-08 15:12:18 +0300

    KS-16-4 смена статуса заказа при помощи отдельного публичного метода

---
+++ src/main/kotlin/lesson16/lesson16_task4.kt
@@ -0,0 +1,24 @@
+package org.example.lesson16
+
+enum class Status {
+    PLACED,
+    IN_PROCESS,
+    READY,
+    CANCELED
+}
+
+class Order(private val id: Int) {
+
+    private var status = Status.PLACED
+
+    private fun changeStatus(newStatus: Status) {
+        status = newStatus
+    }
+
+    fun managerChangeStatus(newStatus: Status) = changeStatus(newStatus)
+}
+
+fun main() {
+    val order = Order(111)
+    order.managerChangeStatus(Status.CANCELED)
+}
