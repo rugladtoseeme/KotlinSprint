36d78faec2cd7f31ebff7f2554d82985952d0f98
rugladtoseeme <mrgrafen@mail.ru>
2025-10-08 16:36:18 +0300

    KS-16-5 сымитировать бой в компьютерной игре

---
+++ src/main/kotlin/lesson16/lesson16_task5.kt
@@ -0,0 +1,47 @@
+package org.example.lesson16
+
+class Player(val name: String, private var hp: Int, var damage: Int) {
+
+    private var isAlive = true
+
+    fun takeDamage(damage: Int) {
+        if (!isAlive) println("Игрок $name не может атаковать, он умер.")
+        else {
+            hp -= damage
+            println("Игрок $name потерял $damage hp, текущее hp = $hp")
+            if (hp <= 0) {
+                die()
+            }
+        }
+    }
+
+    fun heal(hp: Int) {
+        if (!isAlive) println("Игрок $name не может лечиться, он умер.")
+        else {
+            this.hp += hp
+            println("Игрок $name использовал лечение на $hp hp, текущее hp = ${this.hp}")
+        }
+    }
+
+    private fun die() {
+        isAlive = false
+        hp = 0
+        damage = 0
+        println("Игрок $name погиб...")
+    }
+}
+
+fun main() {
+
+    val player = Player("peter", 100, 20)
+
+    player.takeDamage(30)
+    player.heal(20)
+    player.takeDamage(35)
+    player.heal(45)
+    player.takeDamage(60)
+    player.heal(10)
+    player.takeDamage(55)
+
+    player.heal(20)
+}
