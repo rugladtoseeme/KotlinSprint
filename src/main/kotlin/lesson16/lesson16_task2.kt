54ac5230e0d5be1b21f17c00e216c96529b08062
rugladtoseeme <mrgrafen@mail.ru>
2025-10-09 16:58:55 +0300

    KS-16-2 правки по ревью

--- src/main/kotlin/lesson16/lesson16_task2.kt
+++ src/main/kotlin/lesson16/lesson16_task2.kt
@@ -1,13 +1,15 @@
 package org.example.lesson16
 
+import kotlin.math.pow
+
 private const val PI = 3.14
 
-class Circle(private val radius: Int) {
+class Circle(private val radius: Double) {
     fun perimeter() = 2 * PI * radius
-    fun area() = PI * radius * radius
+    fun area() = PI * radius.pow(2)
 }
 
 fun main() {
-    val circle = Circle(5)
+    val circle = Circle(5.0)
     println("площадь круга = ${circle.area()}, длина окружности = ${circle.perimeter()}.")
 }
