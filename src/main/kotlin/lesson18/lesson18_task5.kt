10fb0ac92e7c13a397ebad8e39b06ee38f589962
rugladtoseeme <mrgrafen@mail.ru>
2025-10-13 17:18:39 +0300

    KS-18-5 нарисовать фигуры различных типов на экране

--- src/main/kotlin/lesson18/lesson18_task5.kt
+++ src/main/kotlin/lesson18/lesson18_task5.kt
@@ -2,39 +2,38 @@ package org.example.lesson18
 
 abstract class Figure
 
-class Circle: Figure(){
+class Circle : Figure() {
     override fun toString() = "круг"
 }
 
-class Square: Figure(){
+class Square : Figure() {
     override fun toString() = "квадрат"
 }
 
-class Point: Figure(){
+class Point : Figure() {
     override fun toString() = "точка"
 }
 
-class Screen(){
+class Screen() {
 
-    fun drawFigure(x: Int, y: Int, figure: Figure): String{
-       return "Нарисована фигура: $figure на координатах: ($x, $y)"
-    }
-
-    fun drawFigure(x: Float, y: Float, figure: Figure): String{
+    fun drawFigure(x: Int, y: Int, figure: Figure): String {
         return "Нарисована фигура: $figure на координатах: ($x, $y)"
     }
 
-    fun drawFigure(x: Int, y: Float, figure: Figure): String{
+    fun drawFigure(x: Float, y: Float, figure: Figure): String {
         return "Нарисована фигура: $figure на координатах: ($x, $y)"
     }
 
-    fun drawFigure(x: Float, y: Int, figure: Figure): String{
+    fun drawFigure(x: Int, y: Float, figure: Figure): String {
+        return "Нарисована фигура: $figure на координатах: ($x, $y)"
+    }
+
+    fun drawFigure(x: Float, y: Int, figure: Figure): String {
         return "Нарисована фигура: $figure на координатах: ($x, $y)"
     }
 }
 
 fun main() {
-
     val screen = Screen()
     println(screen.drawFigure(1, 5, Circle()))
     println(screen.drawFigure(1.0f, 5, Circle()))
