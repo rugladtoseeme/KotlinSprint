38fe92361df5fcbf42276160feec0aad9dd8fc47
rugladtoseeme <mrgrafen@mail.ru>
2025-10-15 12:18:27 +0300

    KS-18-2 правки по ревью

--- src/main/kotlin/lesson18/lesson18_task2.kt
+++ src/main/kotlin/lesson18/lesson18_task2.kt
@@ -1,24 +1,24 @@
 package org.example.lesson18
 
-abstract class Dice {
+abstract class Dice(val numberOfFaces: Int) {
     abstract fun throwDice()
 }
 
-class FourFacedDice : Dice() {
+class FourFacedDice : Dice(numberOfFaces = 4) {
     override fun throwDice() {
-        println("На четырехграннике выпало ${(1..4).random()} очков.")
+        println("На четырехграннике выпало ${(1..numberOfFaces).random()} очков.")
     }
 }
 
-class SixFacedDice : Dice() {
+class SixFacedDice : Dice(numberOfFaces = 6) {
     override fun throwDice() {
-        println("На шестиграннике выпало ${(1..6).random()} очков.")
+        println("На шестиграннике выпало ${(1..numberOfFaces).random()} очков.")
     }
 }
 
-class EightFacedDice : Dice() {
+class EightFacedDice : Dice(numberOfFaces = 8) {
     override fun throwDice() {
-        println("На восьмиграннике выпало ${(1..8).random()} очков.")
+        println("На восьмиграннике выпало ${(1..numberOfFaces).random()} очков.")
     }
 }
 
