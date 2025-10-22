2b4153e1557a74447763eec23806c9ab4bd61051
rugladtoseeme <mrgrafen@mail.ru>
2025-10-22 13:10:07 +0300

    KS-21-5 правки по ревью

--- src/main/kotlin/lesson21/lesson21_task5.kt
+++ src/main/kotlin/lesson21/lesson21_task5.kt
@@ -4,13 +4,14 @@ fun Map<String, Int>.maxCategory() = maxByOrNull { it.value }?.key
 
 fun main() {
 
-    val player = mutableMapOf<String, Int>()
-    player.put("Магия", 3)
-    player.put("Стрельба", 5)
-    player.put("Фехтование", 2)
-    player.put("Алхимия", 6)
-    player.put("Харизма", 8)
-    player.put("Ловкость рук", 8)
+    val player = mapOf<String, Int>(
+        "Магия" to 3,
+        "Ловкость рук" to 8,
+        "Алхимия" to 6,
+        "Харизма" to 8,
+        "Фехтование" to 2,
+        "Стрельба" to 5
+    )
 
     println("Ваш наилучший навык - это ${player.maxCategory()}")
 }
