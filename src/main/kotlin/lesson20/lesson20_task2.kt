90e619ebbff8e4e8bcfc569d58de5ffa0f1d0b49
rugladtoseeme <mrgrafen@mail.ru>
2025-10-27 13:02:28 +0300

    правки чтобы не было конфликта имен классов

--- src/main/kotlin/lesson20/lesson20_task2.kt
+++ src/main/kotlin/lesson20/lesson20_task2.kt
@@ -1,15 +1,15 @@
 package org.example.lesson20
 
-class Player(val name: String, val maxHp: Int, var currentHp: Int) {
+class Player1(val name: String, val maxHp: Int, var currentHp: Int) {
     override fun toString() = "Игрок $name, здоровье:($currentHp/$maxHp)"
 }
 
 fun main() {
 
-    val player1 = Player("jean", 100, 35)
+    val player1 = Player1("jean", 100, 35)
     println(player1)
 
-    val useHealingPotion: (Player) -> Unit = { player: Player ->
+    val useHealingPotion: (Player1) -> Unit = { player: Player1 ->
         player.currentHp = player.maxHp
         println("Здоровье восстановлено!")
     }
