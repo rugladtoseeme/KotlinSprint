d013b67f834768f9fa520790358315e3653badcd
rugladtoseeme <mrgrafen@mail.ru>
2025-10-29 12:12:13 +0300

    KS-22-3 правки по ревью

--- src/main/kotlin/lesson22/lesson22_task3.kt
+++ src/main/kotlin/lesson22/lesson22_task3.kt
@@ -4,5 +4,6 @@ data class Cat(val name: String, val age: Int, val furColor: String)
 
 fun main() {
     val cat1 = Cat("Тома", 10, "черная")
-    println("Кошку зовут ${cat1.component1()}, ей ${cat1.component2()} лет, у нее ${cat1.component3()} шерстка.")
+    val (name, age, funColor) = cat1
+    println("Кошку зовут $name, ей $age лет, у нее $funColor шерстка.")
 }
